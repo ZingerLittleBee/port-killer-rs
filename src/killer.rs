469cc511d57866efa18d2e7c541dcf43524505de
ZingerLittleBee <6970999@gmail.com>
2022-05-11 01:56:44 +0800

    feat: 🎸 kill by pid for unix

--- src/killer.rs
+++ src/killer.rs
@@ -1,6 +1,7 @@
 use std::{
+    env,
     io::{self, Error, ErrorKind, Write},
-    process::{Command, Stdio},
+    process::{Command, Output, Stdio},
 };
 
 struct Win;
@@ -8,14 +9,14 @@ struct Unix;
 
 trait Killer {
     fn get_pid(&self, port: u16) -> Result<Vec<u32>, Error>;
-    fn kill(&self, pid: u32) -> Result<bool, Error>;
+    fn kill(&self, pid: Vec<u32>) -> Result<bool, Error>;
 }
 
 impl Killer for Win {
     fn get_pid(&self, port: u16) -> Result<Vec<u32>, Error> {
         todo!()
     }
-    fn kill(&self, pid: u32) -> Result<bool, Error> {
+    fn kill(&self, pid: Vec<u32>) -> Result<bool, Error> {
         todo!()
     }
 }
@@ -46,11 +47,13 @@ impl Killer for Unix {
             .expect("Failed to wait for awk");
         if awk_output.status.success() {
             let res_str = String::from_utf8(awk_output.stdout).expect("Failed to parse awk output");
-            // TODO: remove dup port
-            let res_vec: Vec<u32> = res_str
+            let mut res_vec: Vec<u32> = res_str
                 .split('\n')
                 .filter_map(|s| s.parse::<u32>().ok())
                 .collect();
+            // remove dup values
+            res_vec.sort();
+            res_vec.dedup();
             Ok(res_vec)
         } else {
             Err(io::Error::new(
@@ -59,18 +62,32 @@ impl Killer for Unix {
             ))
         }
     }
-    fn kill(&self, pid: u32) -> Result<bool, Error> {
-        todo!()
+    fn kill(&self, pid: Vec<u32>) -> Result<bool, Error> {
+        let pid_str = pid.iter().map(|&p| p.to_string()).collect::<Vec<String>>();
+        let output = Command::new("kill")
+            .arg("-9")
+            .args(&pid_str)
+            .output()
+            .expect("failed to execute process");
+        return Ok(output.status.success());
     }
 }
 
 #[cfg(test)]
 mod killer_tests {
+    use port_selector::random_free_port;
+
     use super::*;
 
     #[test]
     fn get_pid_it_works() {
-        let killer = Unix;
-        println!("killer.get_pid(5000): {:#?}", killer.get_pid(5000).unwrap());
+        let killer = if cfg!(target_os = "windows") {
+            Box::new(Win) as Box<dyn Killer>
+        } else {
+            Box::new(Unix) as Box<dyn Killer>
+        };
+        let free_port = random_free_port().unwrap();
+        let pids = killer.get_pid(free_port).unwrap();
+        assert_eq!(pids, killer.get_pid(free_port).unwrap());
     }
 }
